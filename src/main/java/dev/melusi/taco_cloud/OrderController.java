0968e96b55b9981e9593a6822f3061f7d4068ab1
Melusi Gumbi <gmbmel003@myuct.ac.za>
2025-03-04 09:45:40 +0200

    Processing form submission: Handling a taco order submission

--- src/main/java/dev/melusi/taco_cloud/OrderController.java
+++ src/main/java/dev/melusi/taco_cloud/OrderController.java
@@ -4,8 +4,10 @@ package dev.melusi.taco_cloud;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Slf4j
 @Controller
@@ -16,5 +18,13 @@ public class OrderController {
     @GetMapping("/current")
     public String orderForm(){
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order,
+                               SessionStatus sessionStatus) {
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+        return "redirect:/";
     }
 }
