c0802e3cc86e8e09c83e876b7c760e6077ef6734
Melusi Gumbi <gmbmel003@myuct.ac.za>
2025-03-05 14:36:52 +0200

    Querying the database with JdbcTemplate

--- src/main/java/dev/melusi/taco_cloud/JdbcIngredientRepository.java
+++ src/main/java/dev/melusi/taco_cloud/JdbcIngredientRepository.java
@@ -16,4 +16,28 @@ public class JdbcIngredientRepository implements IngredientRepository{
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    @Override
+    public Iterable<Ingredient> findAll() {
+        return jdbcTemplate.query(
+                "select id, name, type from Ingredient",
+                this::mapRowToIngredient);
+    }
+
+    @Override
+    public Optional<Ingredient> findById(String id) {
+        List<Ingredient> results = jdbcTemplate.query(
+                "select id, name, type from Ingredient where id=?",
+                this::mapRowToIngredient,
+                id);
+        return results.size() == 0 ?
+                Optional.empty() :
+                Optional.of(results.get(0));
+    }
+
+    private Ingredient mapRowToIngredient(ResultSet row, int rowNum) throws SQLException {
+        return new Ingredient(
+                row.getString("id"),
+                row.getString("name"),
+                Ingredient.Type.valueOf(row.getString("type")));
+    }
 }
