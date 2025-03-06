a5df38c6150fcbd9cc6b7d5410434d943950a36c
Melusi Gumbi <gmbmel003@myuct.ac.za>
2025-03-06 14:22:05 +0200

    Annotating the domain for persistence

--- src/main/java/dev/melusi/taco_cloud/Ingredient.java
+++ src/main/java/dev/melusi/taco_cloud/Ingredient.java
@@ -1,9 +1,18 @@
 package dev.melusi.taco_cloud;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
+@Table
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public final class Ingredient {
+    @Id
     private final String id;
     private final String name;
     private final Type type;
--- src/main/java/dev/melusi/taco_cloud/IngredientRepository.java
+++ src/main/java/dev/melusi/taco_cloud/IngredientRepository.java
@@ -2,8 +2,6 @@ package dev.melusi.taco_cloud;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 
 }
