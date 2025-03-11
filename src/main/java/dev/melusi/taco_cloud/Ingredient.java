69044e62dda74c5fe2b0a9bfa675454062c0fecb
Melusi Gumbi <gmbmel003@myuct.ac.za>
2025-03-11 17:11:15 +0200

    Annotating the domain as entities

--- src/main/java/dev/melusi/taco_cloud/Ingredient.java
+++ src/main/java/dev/melusi/taco_cloud/Ingredient.java
@@ -1,17 +1,14 @@
 package dev.melusi.taco_cloud;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
 
 @Data
-@Table
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
-public final class Ingredient {
+public class Ingredient {
     @Id
     private String id;
     private String name;
--- src/main/java/dev/melusi/taco_cloud/LoadDatabase.java
+++ src/main/java/dev/melusi/taco_cloud/LoadDatabase.java
@@ -4,30 +4,10 @@ import dev.melusi.taco_cloud.Ingredient.Type;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
 
 @Configuration
 public class LoadDatabase {
 
-
-    @Bean
-    public CommandLineRunner dataLoader(JdbcAggregateTemplate template) {
-        return args -> {
-            // JDBC uses the save method as an update method in the database
-            template.insert(new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
-            template.insert(new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP));
-            template.insert(new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
-            template.insert(new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN));
-            template.insert(new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES));
-            template.insert(new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES));
-            template.insert(new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE));
-            template.insert(new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE));
-            template.insert(new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE));
-            template.insert(new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
-        };
-    }
-
-    /*
     @Bean
     public CommandLineRunner dataLoader(IngredientRepository repo) {
          return args -> {
@@ -44,6 +24,6 @@ public class LoadDatabase {
                  repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
              }
         };
-    }*/
+    }
 
 }
