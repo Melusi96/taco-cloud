75988def218a1c3e2842c537a598a0d771888c95
Melusi Gumbi <gmbmel003@myuct.ac.za>
2025-03-12 08:57:28 +0200

    Customizing user authentication - CREATING A USER DETAILS SERVICE

--- src/main/java/dev/melusi/taco_cloud/SecurityConfig.java
+++ src/main/java/dev/melusi/taco_cloud/SecurityConfig.java
@@ -2,16 +2,9 @@ package dev.melusi.taco_cloud;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -22,14 +15,13 @@ public class SecurityConfig {
     }
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder encoder) {
-        List<UserDetails> usersList = new ArrayList<>();
-        usersList.add(new User(
-                "buzz", encoder.encode("password"),
-                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
-        usersList.add(new User(
-                "woody", encoder.encode("password"),
-                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
-        return new InMemoryUserDetailsManager(usersList);
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        //UserDetailsService is a functional interface(single abstract method) and can
+        //be implemented as a lambda instead of as a full-blown implementation class.
+        return username -> {
+            User user = userRepo.findByUsername(username);
+            if (user != null) return user;
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        };
     }
 }
