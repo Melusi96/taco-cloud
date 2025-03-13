a64aa5db52b62a54ac0f558869774757e25517f2
Melusi Gumbi <gmbmel003@myuct.ac.za>
2025-03-13 15:52:10 +0200

    Securing web requests

--- src/main/java/dev/melusi/taco_cloud/SecurityConfig.java
+++ src/main/java/dev/melusi/taco_cloud/SecurityConfig.java
@@ -2,9 +2,11 @@ package dev.melusi.taco_cloud;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -23,5 +25,17 @@ public class SecurityConfig {
             if (user != null) return user;
             throw new UsernameNotFoundException("User '" + username + "' not found");
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/design", "/orders")
+                .hasRole("USER")
+                .requestMatchers("/", "/**")
+                .permitAll()
+            )
+            .build();
     }
 }
