3ec872bc1c832cde5251acd550b6a7b04efadd96
Melusi Gumbi <gmbmel003@myuct.ac.za>
2025-04-08 06:26:42 +0200

    A RESTful controller for taco design API requests

--- src/main/java/dev/melusi/taco_cloud/TacoController.java
+++ src/main/java/dev/melusi/taco_cloud/TacoController.java
@@ -2,10 +2,11 @@ package dev.melusi.taco_cloud;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/tacos", produces = "application/json") //Handles requests for /api/tacos
@@ -23,5 +24,15 @@ public class TacoController {
                 0, 12,
                 Sort.by("createdAt").descending());
         return tacoRepo.findAll(page).getContent();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id){
+        Optional<Taco> optTaco = tacoRepo.findById(id);
+
+        if(optTaco.isPresent()){
+            return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 }
